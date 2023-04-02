4b650dd80dd50940c4ec4d052f9ace3c409ccf16
Tim Dortmann <git@tdortman.com>
2023-04-02 05:28:31 +0200

    revert last commit

--- src/handlers/messages.ts
+++ src/handlers/messages.ts
@@ -96,5 +96,4 @@ async function handleCommand({ command, subCmd, message, prefix }: MessageComman
     else if (command === "py") await misc.consoleCmd(message, undefined, true);
     else if (command === "someone") await misc.pingRandomMembers(message);
     else if (command === "yoink") await emoji.addEmoji(message, prefix);
-    else if (command === "db") await db.runSQL(message);
 }
