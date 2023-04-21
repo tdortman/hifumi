df677f69a17b509677caa9f640101addd194cdc1
Tim Dortmann <git@tdortman.com>
2023-04-21 03:03:06 +0200

    db migration script

---
+++ src/db/migrate.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { migrate } from "drizzle-orm/planetscale-serverless/migrator.js";
+import { db } from "../app.js";
+
+// this will automatically run needed migrations on the database
+migrate(db, { migrationsFolder: "./migrations" })
+    .then(() => {
+        console.log("Migrations complete!");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error("\nMigrations failed!\n", err);
+        process.exit(1);
+    });
