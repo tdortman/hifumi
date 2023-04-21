80a26263729afcc30d9b5499ab43e919036360e6
Tim Dortmann <git@tdortman.com>
2023-04-21 03:19:06 +0200

    add breakpoints to fix migrate script

--- migrations/0000_thick_bucky.sql
+++ migrations/0000_thick_bucky.sql
@@ -9,37 +9,37 @@ CREATE TABLE `error_logs` (
 	`log` text,
 	`error` text
 );
-
+--> statement-breakpoint
 CREATE TABLE `help_messages` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`cmd` varchar(100) NOT NULL,
 	`desc` varchar(255) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `leet` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`source` char(1) NOT NULL,
 	`translated` varchar(10) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `miku_command_aliases` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`command` varchar(25) NOT NULL,
 	`alias` varchar(25) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `miku_reactions` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`command` varchar(50) NOT NULL,
 	`reaction` varchar(255) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `prefixes` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`server_id` varchar(25) NOT NULL,
 	`prefix` varchar(255) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `reddit_posts` (
 	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`subreddit` varchar(50) NOT NULL,
@@ -48,14 +48,15 @@ CREATE TABLE `reddit_posts` (
 	`over_18` boolean NOT NULL,
 	`permalink` varchar(255) NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `statuses` (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`type` varchar(20) NOT NULL,
 	`status` varchar(255) NOT NULL
 );
+--> statement-breakpoint
 ALTER TABLE `statuses` ADD PRIMARY KEY(`type`,`status`);
-
-CREATE UNIQUE INDEX `server_id` ON `prefixes` (`server_id`);
-CREATE INDEX `subreddit` ON `reddit_posts` (`subreddit`);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `server_id` ON `prefixes` (`server_id`);--> statement-breakpoint
+CREATE INDEX `subreddit` ON `reddit_posts` (`subreddit`);--> statement-breakpoint
 CREATE INDEX `id` ON `statuses` (`id`);
