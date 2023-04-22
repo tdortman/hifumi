bee5997a0b6e5a821e62ce4f20c7dfe6ad713bc9
Tim Dortmann <git@tdortman.com>
2023-04-22 03:56:56 +0200

    make sure to destroy discord client when migrating db

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import { migrate } from "drizzle-orm/planetscale-serverless/migrator.js";
-import { db } from "../app.js";
+import { client, db } from "../app.js";
 
 // this will automatically run needed migrations on the database
 migrate(db, { migrationsFolder: "./migrations" })
     .then(() => {
         console.log("\nMigrations complete!");
+        client.destroy();
         process.exit(0);
     })
     .catch((err) => {
         console.error("\nMigrations failed!\n", err);
+        client.destroy();
         process.exit(1);
     });
