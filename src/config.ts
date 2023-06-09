1edba596de8a95db1183854709b87ee7036f0815
Tim Dortmann <git@tdortman.com>
2023-06-09 23:16:46 +0200

    update owner username to use new system

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
 
 export const BOT_OWNERS = ["258993932262834188", "207505077013839883"];
-export const OWNER_USERNAME = "Tilted Toast#0001";
+export const OWNER_USERNAME = "@toast.dll";
 export const EMBED_COLOUR = "#CE3A9B";
 export const DEV_CHANNELS = ["655484859405303809", "551588329003548683", "922679249058553857"];
 export const LOG_CHANNEL = "655484804405657642";
