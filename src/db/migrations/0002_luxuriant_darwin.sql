5fdd7ba0e1d4d26be1f4fd92b8538e611c137401
Tim Dortmann <git@tdortman.com>
2026-03-25 17:41:58 +0100

    fix migration

--- src/db/migrations/0002_luxuriant_darwin.sql
+++ src/db/migrations/0002_luxuriant_darwin.sql
@@ -1,12 +1,15 @@
-CREATE TABLE `nixpkgs_pr_subscriptions` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`user_id` text(255) NOT NULL,
-	`pr_number` integer NOT NULL,
-	`branch` text(255) DEFAULT 'nixos-unstable' NOT NULL,
-	`channel_id` text(255),
-	`merge_commit_sha` text(255),
-	`created_at` text DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS `nixpkgs_pr_subscriptions` (
+    `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `user_id` text(255) NOT NULL,
+    `pr_number` integer NOT NULL,
+    `branch` text(255) DEFAULT 'nixos-unstable' NOT NULL,
+    `channel_id` text(255),
+    `merge_commit_sha` text(255),
+    `created_at` text DEFAULT CURRENT_TIMESTAMP
 );
+
 --> statement-breakpoint
-CREATE INDEX `nixpkgs_user_idx` ON `nixpkgs_pr_subscriptions` (`user_id`);--> statement-breakpoint
-CREATE UNIQUE INDEX `unique_user_pr_branch` ON `nixpkgs_pr_subscriptions` (`user_id`,`pr_number`,`branch`);
+CREATE INDEX IF NOT EXISTS `nixpkgs_user_idx` ON `nixpkgs_pr_subscriptions` (`user_id`);
+
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS `unique_user_pr_branch` ON `nixpkgs_pr_subscriptions` (`user_id`, `pr_number`, `branch`);
