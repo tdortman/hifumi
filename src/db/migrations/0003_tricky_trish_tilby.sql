aec06a01b7a7f60510e3545d623f634f4e5b9774
Tim Dortmann <git@tdortman.com>
2026-05-01 02:08:25 +0200

    fix: ensure indexes are created only if they do not exist in nixpkgs_branch_subscriptions table

--- src/db/migrations/0003_tricky_trish_tilby.sql
+++ src/db/migrations/0003_tricky_trish_tilby.sql
@@ -1,11 +1,14 @@
-CREATE TABLE `nixpkgs_branch_subscriptions` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`user_id` text(255) NOT NULL,
-	`branch` text(255) DEFAULT 'nixos-unstable' NOT NULL,
-	`channel_id` text(255),
-	`last_seen_sha` text(255) NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS `nixpkgs_branch_subscriptions` (
+    `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `user_id` text(255) NOT NULL,
+    `branch` text(255) DEFAULT 'nixos-unstable' NOT NULL,
+    `channel_id` text(255),
+    `last_seen_sha` text(255) NOT NULL,
+    `created_at` text DEFAULT CURRENT_TIMESTAMP
 );
+
 --> statement-breakpoint
-CREATE INDEX `nixpkgs_branch_user_idx` ON `nixpkgs_branch_subscriptions` (`user_id`);--> statement-breakpoint
-CREATE UNIQUE INDEX `unique_user_branch` ON `nixpkgs_branch_subscriptions` (`user_id`,`branch`);
+CREATE INDEX IF NOT EXISTS `nixpkgs_branch_user_idx` ON `nixpkgs_branch_subscriptions` (`user_id`);
+
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS `unique_user_branch` ON `nixpkgs_branch_subscriptions` (`user_id`, `branch`);
